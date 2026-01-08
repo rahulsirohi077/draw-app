cc59b45e575523817e6382ad208e1409129d092f
rahulsirohi077 <rahulsirohi077@gmail.com>
2026-01-08 19:23:09 +0530

    Add db Package and integrate with http-backend

---
+++ apps/http-backend/src/index.ts
@@ -0,0 +1,61 @@
+import express from "express";
+import { JWT_SECRET } from "@repo/backend-common/config";
+import jwt from "jsonwebtoken";
+import { middleware } from "./middleware";
+import { CreateUserSchema,SigninSchema,CreateRoomSchema } from "@repo/common/types";
+import { prismaClient } from "@repo/db/client";
+
+const app = express();
+
+app.post('/signup',(req,res) => {
+    const parsedData = CreateUserSchema.safeParse(req.body);
+    if(!parsedData.success){
+        return res.json({
+            message:"Incorrect Inputs"
+        })
+    }
+
+    prismaClient.user.create({
+        data:{
+            email: parsedData.data?.username,
+            password: parsedData.data?.password,
+            name: parsedData.data.name
+        }
+    })
+    res.json({
+        userId:123
+    })
+})
+
+app.post('/signin',(req,res)=>{
+    const data = SigninSchema.safeParse(req.body);
+    if(!data.success){
+        return res.json({
+            message:"Incorrect Inputs"
+        })
+    }
+    const userId = 1;
+    const token = jwt.sign({
+        userId
+    },JWT_SECRET)
+
+    res.json({
+        token
+    })
+})
+
+app.post('/room',middleware,(req,res)=>{
+    const data = CreateRoomSchema.safeParse(req.body);
+    if(!data.success){
+        return res.json({
+            message:"Incorrect Inputs"
+        })
+    }
+    // db call
+
+    res.json({
+        roomId:123
+    })
+})
+
+app.listen(3001)
